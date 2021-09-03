f3b46be3581b592c7deafced08d384791319326c
Sergio <bostshakur@gmail.com>
2021-09-03 06:14:05 -0400

    Attempt to fix

--- app/__init__.py
+++ app/__init__.py
@@ -1,25 +1,14 @@
 from flask import Flask
-from config import Config
-import os
-app = Flask(__name__)
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from config import Config
 
-SECRET_KEY = os.urandom(32)
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-SQLACHEMY_DATABASE_URL = os.environ.get("DATABASE_URL") or 'sqlite:///' + os.path.join(basedir, 'app.db')
-SQLACHEMY_TRACK_MODIFICATIONS = False
+app = Flask(__name__)
+app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///app/db"
-# app.config.from_object(Config)
-
-
 
 from app import routes, models
-
 
 
 
--- app/config.py
+++ app/config.py
@@ -1,19 +1,9 @@
-from os import environ, path
-from dotenv import load_dotenv
-
-basedir = path.abspath(path.dirname(__file__))
-load_dotenv(path.join(basedir, '.env'))
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 
-class Config:
-    """Set Flask configuration from .env file."""
-
-    # General Config
-    SECRET_KEY = environ.get('SECRET_KEY')
-    FLASK_APP = environ.get('FLASK_APP')
-    FLASK_ENV = environ.get('FLASK_ENV')
-
-    # Database
-    SQLALCHEMY_DATABASE_URI = environ.get("sqlite:///app.db")
-    SQLALCHEMY_ECHO = False
+class Config(object):
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
